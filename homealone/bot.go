f53e67ea11b231c28df5b6045fac614d8c69bbe6
Alexander Kramarev <pochemuto@yandex-team.ru>
2024-02-28 21:08:15 +0000

    Shutdown

--- homealone/bot.go
+++ homealone/bot.go
@@ -3,6 +3,7 @@ package homealone
 import (
 	"log"
 	"os"
+	"strconv"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
@@ -23,6 +24,18 @@ func Message(incomming tgbotapi.Update, text string) tgbotapi.MessageConfig {
 	msg := tgbotapi.NewMessage(incomming.Message.Chat.ID, text)
 	msg.ReplyToMessageID = incomming.Message.MessageID
 	return msg
+}
+
+func NotAuthorizedUser(incoming tgbotapi.Update) tgbotapi.MessageConfig {
+	return Message(incoming, "Пользователь "+incoming.SentFrom().UserName+" не авторизован")
+}
+
+func IsAuthorized(user tgbotapi.User) bool {
+	id, err := strconv.ParseInt(os.Getenv("AUTHORIZED_USER_ID"), 10, 64)
+	if err != nil {
+		log.Panic(err)
+	}
+	return user.ID == id
 }
 
 func Start() {
@@ -50,12 +63,29 @@ func Start() {
 
 			switch update.Message.Command() {
 			case "wakeup":
-				err = Wakeup()
-				if err == nil {
-					bot.Send(Message(update, "Пробуждаем"))
-				} else {
-					bot.Send(ErrorMessage(update, err))
+				if !IsAuthorized(*update.SentFrom()) {
+					bot.Send(NotAuthorizedUser(update))
+					continue
 				}
+				err = Wakeup()
+				if err != nil {
+					bot.Send(ErrorMessage(update, err))
+					continue
+				}
+
+				bot.Send(Message(update, "Пробуждаем"))
+			case "shutdown":
+				if !IsAuthorized(*update.SentFrom()) {
+					bot.Send(NotAuthorizedUser(update))
+					continue
+				}
+				err = Shutdown()
+				if err != nil {
+					bot.Send(ErrorMessage(update, err))
+					continue
+				}
+
+				bot.Send(Message(update, "Выключаем"))
 			default:
 				bot.Send(Echo(update))
 			}
--- homealone/plex.go
+++ homealone/plex.go
@@ -1,12 +1,33 @@
 package homealone
 
 import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net"
+	"net/http"
 	"os"
 
 	"github.com/mdlayher/wol"
 )
+
+func Shutdown() error {
+	trigger := os.Getenv("PLEX_SHUTDOWN_TRIGGER")
+	body := []byte(os.Getenv("ZINA_SECRET"))
+	resp, err := http.Post(trigger, "Content-Type: text/plain", bytes.NewReader(body))
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != 200 {
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return err
+		}
+		return fmt.Errorf("failed response (%d): %s", resp.StatusCode, body)
+	}
+	return nil
+}
 
 func Wakeup() error {
 	client, err := wol.NewClient()
@@ -15,7 +36,7 @@ func Wakeup() error {
 	}
 	defer client.Close()
 
-	ip := os.Getenv("PLEX_HOST")
+	ip := os.Getenv("WAKEUP_BROADCAST_ADDRESS")
 	mac, err := net.ParseMAC(os.Getenv("PLEX_MAC"))
 	if err != nil {
 		return err

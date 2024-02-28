92152a25806e14250c03baa4f9eba0af621c5df8
Alexander Kramarev <pochemuto@yandex-team.ru>
2024-02-28 19:38:47 +0000

    Simple bot implementation

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/pochemuto/homealone-go
 
 go 1.22.0
+
+require github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1 // indirect
--- homealone/bot.go
+++ homealone/bot.go
@@ -2,14 +2,38 @@ package homealone
 
 import (
 	"log"
-	"time"
+	"os"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 func Start() {
-	timer := time.NewTicker(1 * time.Second)
-	var i = 0
-	for range timer.C {
-		i++
-		log.Println("Tick", i)
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_TOKEN"))
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	_, err = bot.MakeRequest("deleteWebhook", tgbotapi.Params{})
+	if err != nil {
+		log.Panic(err)
+	}
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message != nil { // If we got a message
+			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+			msg.ReplyToMessageID = update.Message.MessageID
+
+			bot.Send(msg)
+		}
 	}
 }

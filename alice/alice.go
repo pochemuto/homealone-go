7d809e8da5464bf24eead3a4645677e7b05f91cc
Alexander Kramarev <pochemuto@yandex-team.ru>
2024-10-21 18:24:56 +0200

    Add http handler

--- alice/alice.go
+++ alice/alice.go
@@ -1,9 +1,16 @@
 package alice
 
-import "context"
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
 
 type Alice struct {
-	ctx context.Context
+	ctx    context.Context
+	server *http.Server
 }
 
 func NewAlice() Alice {
@@ -13,5 +20,43 @@ func NewAlice() Alice {
 func (a Alice) Start(ctx context.Context) error {
 	a.ctx = ctx
 
+	port := os.Getenv("ALICE_PORT")
+	// Создаем новый HTTP сервер
+	a.server = &http.Server{
+		Addr:    ":" + port,
+		Handler: a.routes(),
+	}
+
+	go func() {
+		log.Printf("HTTP server started on :%s\n", port)
+		if err := a.server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Could not start server: %v", err)
+		}
+	}()
+
+	// Ожидаем завершения через контекст
+	<-ctx.Done()
+
+	// Завершаем работу сервера при получении сигнала завершения
+	log.Println("Shutting down server...")
+
+	if err := a.server.Shutdown(context.Background()); err != nil {
+		log.Fatalf("Server Shutdown Failed:%+v", err)
+	}
+
+	log.Println("Server exited properly")
+
 	return nil
+}
+
+func (a *Alice) routes() http.Handler {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", a.handleHome())
+	return mux
+}
+
+func (a *Alice) handleHome() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello from Alice!")
+	}
 }

9b17cf434b6c96108c4058042b4a1941ac32a41f
Alexander Kramarev <pochemuto@yandex-team.ru>
2024-11-02 16:34:38 +0100

    Hours ago

--- homealone/bot.go
+++ homealone/bot.go
@@ -131,8 +131,10 @@ func getVersion() (string, error) {
 	duration := time.Since(buildTime)
 	if duration < time.Minute {
 		return fmt.Sprintf("%s, %d seconds ago", buildDate, int(duration.Seconds())), nil
-	} else if duration < time.Hour*24 {
+	} else if duration < time.Hour {
 		return fmt.Sprintf("%s, %d minutes ago", buildDate, int(duration.Minutes())), nil
+	} else if duration < time.Hour*24 {
+		return fmt.Sprintf("%s, %d hours ago", buildDate, int(duration.Hours())), nil
 	} else {
 		return fmt.Sprintf("%s, %d days ago", buildDate, int(duration.Hours()/24)), nil
 	}
